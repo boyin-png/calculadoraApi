a77179f9b28f1bb05a5560cc6f5d64630f36344c
boyin-png <faride2405@gmail.com>
2025-07-10 12:31:36 -0600

    caraphermelia

--- src/api/routes/calculator.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const calculatorController = require('../controllers/calculator.controller.js');
-
-router.post('/rectangle/area', calculatorController.calculateRectangleArea);
-router.post('/cube/volume', calculatorController.calculateCubeVolume);
-router.post('/cylinder/volume', calculatorController.calculateCylinderVolume);
-
-module.exports = router;
