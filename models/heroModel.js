100dca911748b659daee4915a8480dc966f2974f
boyin-png <faride2405@gmail.com>
2025-07-24 09:36:05 -0600

    the jaws of life

--- models/heroModel.js
+++ models/heroModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const heroSchema = new mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId, // Guarda el ID del USUARIO
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
     },
@@ -10,12 +10,21 @@ const heroSchema = new mongoose.Schema({
     power: { type: String, required: true },
     age: { type: Number, required: true },
     city: { type: String, required: true },
-    pets: [{ // Guarda una lista de IDs de MASCOTAS
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pet'
-    }],
-    coins: { type: Number, default: 50 }
-}, { timestamps: true });
+    
+    // --- ESTA ES LA CORRECCIÓN CLAVE ---
+    pets: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Pet' }],
+        default: [] // Asegura que este campo sea SIEMPRE un array, aunque esté vacío
+    },
+    // -----------------------------------
+
+    coins: {
+        type: Number,
+        default: 50
+    }
+}, {
+    timestamps: true
+});
 
 const Hero = mongoose.model('Hero', heroSchema);
 export default Hero;
